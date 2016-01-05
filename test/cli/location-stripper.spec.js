e44b8d8d4e4e362d6fb990e65edaca0a5677fb74
Michael Demmer <demmer@jut.io>
2016-01-05 07:03:51 -0800

    cli: strip locations when in flowgraph mode
    Update the location stripper to support either an AST or a flowgraph
    and to strip the locations from either.
    
    Along the way update the unit test to make sure that the AST actually
    had a location property in the expected places before the stripper
    runs to prove the stripper actually did something to the AST.t

--- test/cli/location-stripper.spec.js
+++ test/cli/location-stripper.spec.js
@@ -1,15 +1,25 @@
 var expect = require('chai').expect;
-var parser = require('../../lib/parser');
+var compiler = require('../../lib/compiler');
 
-var LocationStripper = require('../../lib/cli/location-stripper');
+var locationStripper = require('../../lib/cli/location-stripper');
 
 describe('LocationStripper', function() {
     describe('strip', function() {
-        it('strips location info from all nodes', function() {
-            var ast = parser.parseSync('emit -limit 5');
-            var stripper = new LocationStripper();
+        it('strips location info from all ast nodes', function() {
+            var ast = compiler.compileSync('emit -limit 5', {stage: 'parse'});
 
-            stripper.strip(ast);
+            // MainModuleDef
+            expect(ast).to.have.property('location');
+            // SequentialGraph
+            expect(ast.elements[0]).to.have.property('location');
+            // OptionOnlyProc(name = "emit")
+            expect(ast.elements[0].elements[0]).to.have.property('location');
+            // ProcOption(id = "limit")
+            expect(ast.elements[0].elements[0].options[0]).to.have.property('location');
+            // NumericLiteral(value = 5)
+            expect(ast.elements[0].elements[0].options[0].expr).to.have.property('location');
+
+            locationStripper(ast);
 
             // MainModuleDef
             expect(ast).not.to.have.property('location');
@@ -21,6 +31,18 @@ describe('LocationStripper', function() {
             expect(ast.elements[0].elements[0].options[0]).not.to.have.property('location');
             // NumericLiteral(value = 5)
             expect(ast.elements[0].elements[0].options[0].expr).not.to.have.property('location');
+        });
+
+        it('strips location info from all flowgraph nodes', function() {
+            var graph = compiler.compileSync('emit -limit 5 | view table', {stage: 'flowgraph'});
+
+            expect(graph.built_graph.nodes[0]).to.have.property('location');
+            expect(graph.built_graph.nodes[1]).to.have.property('location');
+
+            locationStripper(graph);
+
+            expect(graph.built_graph.nodes[0]).to.not.have.property('location');
+            expect(graph.built_graph.nodes[1]).to.not.have.property('location');
         });
     });
 });
