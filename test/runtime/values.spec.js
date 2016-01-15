accdd95bdcf0d9378bd39f573978a184eff37800
Balazs Kutil <balazs@jut.io>
2016-01-15 13:07:50 +0100

    Add test for juttle moment comparison

--- test/runtime/values.spec.js
+++ test/runtime/values.spec.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var _ = require('underscore');
 var values = require('../../lib/runtime/values');
+var JuttleMoment = require('../../lib/moment').JuttleMoment;
 
 describe('Values tests', function () {
     describe('compare', function() {
@@ -39,6 +40,95 @@ describe('Values tests', function () {
                 expect(values.compare(test.left, test.right)).to.equal(
                     test.result,
                     JSON.stringify(test.left) + ' <=> ' + JSON.stringify(test.right)
+                );
+            });
+        });
+
+        it('moment', function() {
+            var tests = [
+                {
+                    left: new JuttleMoment(1),
+                    right: new JuttleMoment(0),
+                    result: 1
+                },
+                {
+                    left: new JuttleMoment(0),
+                    right: new JuttleMoment(0),
+                    result: 0
+                },
+                {
+                    left: new JuttleMoment(0),
+                    right: new JuttleMoment(1),
+                    result: -1
+                },
+                {
+                    left: new JuttleMoment({ raw: 0, epsilon: true }),
+                    right: new JuttleMoment({ raw: 0, epsilon: true  }),
+                    result: 0
+                },
+                {
+                    left: new JuttleMoment({ raw: 0, epsilon: false }),
+                    right: new JuttleMoment({ raw: 0, epsilon: false }),
+                    result: 0
+                },
+                {
+                    left: new JuttleMoment({ raw: 0, epsilon: true }),
+                    right: new JuttleMoment({ raw: 0, epsilon: false }),
+                    result:  -1
+                },
+                {
+                    left: new JuttleMoment({ raw: 0, epsilon: false }),
+                    right: new JuttleMoment({ raw: 0, epsilon: true }),
+                    result: 1
+                },
+                {
+                    left: new JuttleMoment(Infinity),
+                    right: new JuttleMoment(Infinity),
+                    result: 0
+                },
+                {
+                    left: new JuttleMoment(-Infinity),
+                    right: new JuttleMoment(-Infinity),
+                    result: 0
+                },
+                {
+                    left: new JuttleMoment(Infinity),
+                    right: new JuttleMoment(-Infinity),
+                    result: 1
+                },
+                {
+                    left: new JuttleMoment(-Infinity),
+                    right: new JuttleMoment(Infinity),
+                    result: -1
+                },
+                {
+                    left: new JuttleMoment({ raw: Infinity, epsilon: true }),
+                    right: new JuttleMoment({ raw: Infinity, epsilon: false }),
+                    result: -1
+                },
+                {
+                    left: new JuttleMoment({ raw: Infinity, epsilon: false }),
+                    right: new JuttleMoment({ raw: Infinity, epsilon: true }),
+                    result: 1
+                },
+                {
+                    left: new JuttleMoment({ raw: -Infinity, epsilon: true }),
+                    right: new JuttleMoment({ raw: -Infinity, epsilon: false }),
+                    result: -1
+                },
+                {
+                    left: new JuttleMoment({ raw: -Infinity, epsilon: false }),
+                    right: new JuttleMoment({ raw: -Infinity, epsilon: true }),
+                    result: 1
+                },
+            ];
+
+            _.each(tests, function(test) {
+                expect(values.compare(test.left, test.right)).to.equal(
+                    test.result,
+                    test.left.valueOf() + ' (eps: ' + test.left.epsilon + ')'
+                    + ' <=> '
+                    + test.right.valueOf() + ' (eps: ' + test.right.epsilon + ')'
                 );
             });
         });
