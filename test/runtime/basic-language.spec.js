0d5c1156d876ec72eba62b26195672e8c3e0654f
David Majda <dmajda@jut.io>
2016-03-09 19:07:45 +0100

    SemanticPass: Refactor sa_FunctionProc
    The idea is the same as in refactoring of sa_CallExpression -- rely on
    sa_Variable and sa_MemberExpression for symbol processing and avoid
    duplicating it in sa_FunctionProc.
    
    A nice side effect of the change is fixing an incorrect error message.

--- test/runtime/basic-language.spec.js
+++ test/runtime/basic-language.spec.js
@@ -296,7 +296,7 @@ describe('Juttle basic language tests', function() {
         })
         .catch(function(err) {
             expect(err.name).to.equal('CompileError');
-            expect(err.message).to.match(/.* not exported by/);
+            expect(err.message).to.match(/.* is not defined/);
         });
     });
 
