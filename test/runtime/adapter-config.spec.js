8163229d305babf66f10907b9e686019b6234646
Vladimir Bukhin <vlad@jut.io>
2016-03-22 19:42:03 -0700

    better testing for adapter path

--- test/runtime/adapter-config.spec.js
+++ test/runtime/adapter-config.spec.js
@@ -7,25 +7,36 @@ var expect = require('chai').expect;
 var _ = require('underscore');
 var adapters = require('../../lib/runtime/adapters');
 
+var PATH_TO_TEST_ADAPTER_CONF = './test/runtime/test-adapter-conf';
+var ADAPTER_MODULE_RETRIEVER_FUNCTION = adapters.adapterModulePath;
 
-function configure(name, conf) {
-    conf = _.clone(conf);
-    conf.path = path.resolve(__dirname, '../runtime/test-adapter-conf');
-    
+function getAdapterModulePathFunction(forceConfigAdapter) {
+    if (forceConfigAdapter) {
+        return () => { return path.resolve(__dirname, '../runtime/test-adapter-conf'); };
+    }
+    return ADAPTER_MODULE_RETRIEVER_FUNCTION;
+}
+
+function configure(name, conf, forceAdapterPath) {
     var configurePayload = {};
     configurePayload[name] = conf;
     
+    adapters.adapterModulePath = getAdapterModulePathFunction(forceAdapterPath);
     adapters.configure(configurePayload);
+}
+
+function getTestHostName() {
+    return 'testHost' + Math.random().toString(36).slice(2,10);
 }
 
 describe('init and read from config', function () {
     
     it('load single object', function() {
         var inputConf = {
-            host: 'testHostA'
+            host: getTestHostName()
         };
         var name = inputConf.host;
-        configure(name, inputConf);
+        configure(name, inputConf, true);
         
         return check_juttle({
             program: `read ${name}`
@@ -38,19 +49,39 @@ describe('init and read from config', function () {
         });
     });
     
+    it('load single object with path', function() {
+        var inputConf = {
+            host: getTestHostName(),
+            path: PATH_TO_TEST_ADAPTER_CONF
+        };
+        var name = inputConf.host;
+        configure(name, inputConf);
+        
+        return check_juttle({
+            program: `read ${name}`
+        })
+        .then(function(result) {
+            expect(result.sinks.table).to.have.length(1);
+            
+            var outputConf = result.sinks.table;
+            var expectedOutput = _.omit(inputConf, 'path');
+            expect(outputConf[0]).to.deep.equal(expectedOutput);
+        });
+    });
+    
     it('load array', function() {
         var inputConf = [
             {
                 id: 'first',
-                host: 'testHost1'
+                host: getTestHostName()
             },
             {
                 id: 'second',
-                host: 'testHost2'
+                host: getTestHostName()
             }
         ];
         var name = inputConf[0].host;
-        configure(name, inputConf);
+        configure(name, inputConf, true);
         
         return check_juttle({
             program: `read ${name}`
@@ -68,13 +99,14 @@ describe('init and read from config', function () {
             instances: [
                 {
                     id: 'first',
-                    host: 'testHost3'
+                    host: getTestHostName()
                 },
                 {
                     id: 'second',
-                    host: 'testHost4'
+                    host: getTestHostName()
                 }
-            ] 
+            ],
+            path: PATH_TO_TEST_ADAPTER_CONF
         };
         var name = inputConf.instances[0].host;
         configure(name, inputConf);
@@ -93,15 +125,15 @@ describe('init and read from config', function () {
     it('err missing id', function() {
         var inputConf = [
             {
-                host: 'testHost5'
+                host: getTestHostName()
             },
             {
                 id: 'second',
-                host: 'testHost6'
+                host: getTestHostName()
             }
         ];
         var name = inputConf[0].host;
-        configure(name, inputConf);
+        configure(name, inputConf, true);
         
         return check_juttle({
             program: `read ${name}`
@@ -110,7 +142,7 @@ describe('init and read from config', function () {
             throw new Error('unexpected success');
         })
         .catch(function(err) {
-            expect(err.message).equal(`One or more configuation instances for adapter ${name} is missing an id.`);
+            expect(err.message).equal(`One or more configuration instances for adapter ${name} is missing an id.`);
         });
     });
     
@@ -118,12 +150,40 @@ describe('init and read from config', function () {
         var inputConf = { 
             instances: {
                 id: 'first',
-                host: 'testHost7'
+                host: getTestHostName()
             },
-            host: 'hostPlaceholder1'
+            host: getTestHostName()
         };
         
         var name = inputConf.host;
+        configure(name, inputConf, true);
+        
+        return check_juttle({
+            program: `read ${name}`
+        })
+        .then(function(result) {
+            throw new Error('unexpected success');
+        })
+        .catch(function(err) {
+            expect(err.message).equal(`instances key in configuration for adapter ${name} must be an array.`);
+        });
+    });
+    it('err path not indicated err', function() {
+        var inputConf = { 
+            instances: [
+                {
+                    id: 'first',
+                    host: getTestHostName()
+                },
+                {
+                    id: 'second',
+                    host: getTestHostName()
+                }
+            ],
+            path: './wrong-path/' 
+        };
+        
+        var name = inputConf.instances[0].host;
         configure(name, inputConf);
         
         return check_juttle({
@@ -133,7 +193,8 @@ describe('init and read from config', function () {
             throw new Error('unexpected success');
         })
         .catch(function(err) {
-            expect(err.message).equal(`instances key in configuation for adapter ${name} must be an array.`);
+            expect(err.message).to.contain('Cannot find module');
+            expect(err.message).to.contain('wrong-path/');
         });
     });
 });
